04b1bd3c29c5137518dcb3a2dd03b5bea1db3063
Your Name <you@example.com>
2016-04-07 18:43:16 -0400

    Include test server with routing

---
+++ test_server.js
@@ -0,0 +1,31 @@
+process.title = 'test-server';
+
+var fs = require('fs');
+var hapi = require('hapi');
+var inert   = require('inert');
+
+var root = fs.readFileSync(__dirname + '/layout_partials/root.hbt').toString();
+
+var server = new hapi.Server();
+
+server.connection({
+  host: '0.0.0.0',
+  port: 8080
+});
+
+server.register(inert, function() {});
+
+server.route({
+  method: 'GET',
+  path: root + '/{param*}',
+  handler: {
+    directory: {
+      path: __dirname + '/http',
+      index: true
+    }
+  }
+});
+
+server.start(function () {
+  console.log('Server running at: ' + server.info.uri);
+});
